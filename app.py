94a7f85f078029610c32f4cac8e0cfef9b22ff57
Brenno Lemos <brenno@syndel.is>
2021-04-08 18:34:28 -0300

    Começando o index

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ cursor = conn.cursor()
 def index():
 	return render_template('index.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error = None
@@ -27,11 +28,8 @@ def login():
 
 		error = 'Credenciais Inválidos!'
 
-		# if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-		# 	error = 'Invalid Credentials. Please try again.'
-		# else:
-		# 	return redirect('/')
 	return render_template('login.html', error=error)
+
 
 if __name__ == '__main__':
 	app.run()
