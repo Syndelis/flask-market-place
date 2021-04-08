878d6039038cf34410bfeddc48ef01622b6f7003
Brenno Lemos <brenno@syndel.is>
2021-04-08 14:51:14 -0300

    Adicionado checagem com a base de dados no login

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template, redirect, url_for, request
 import MySQLdb as sql
 
 app = Flask(__name__)
+conn = sql.connect(
+	'alexandrum.go.ro',
+	'tpbd', 'lcrocha',
+	'tpbd'
+)
 
+cursor = conn.cursor()
 
 @app.route('/')
 def index():
@@ -12,11 +18,19 @@ def index():
 def login():
 	error = None
 	if request.method == 'POST':
-		print(f'{request.form}')
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid Credentials. Please try again.'
-		else:
-			return redirect('/')
+
+		name, pswd = request.form['username'], request.form['password']
+
+		cursor.execute("SELECT nome, senha FROM PESSOA WHERE tipo=1;")
+		for row in cursor.fetchall():
+			if (name, pswd) == row: return redirect('/')
+
+		error = 'Credenciais Inválidos!'
+
+		# if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+		# 	error = 'Invalid Credentials. Please try again.'
+		# else:
+		# 	return redirect('/')
 	return render_template('login.html', error=error)
 
 if __name__ == '__main__':
