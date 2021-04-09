525c6561ba25ed60ae15c39c1920710db1758097
Brenno Lemos <brenno@syndel.is>
2021-04-09 00:26:30 -0300

    Checkboxes inteligentes

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,12 @@
 // some scripts
 
 // jquery ready start
+
+const AZ = document.getElementById("A-Z");
+const ZA = document.getElementById("Z-A");
+const PU = document.getElementById("Preço+");
+const PD = document.getElementById("Preço-");
+
 $(document).ready(function() {
 	// jQuery code
 
@@ -18,6 +24,18 @@ $(document).ready(function() {
       e.stopPropagation();
     });
 
+    $(document).on('click', '.smart-checkbox', function (e) {
+        // console.log($(this).attr("id"));
+        var name = $(this).attr("id");
+
+        if ($(this).is(':checked')) {
+            if      (name === "A-Z")    ZA.checked = false;
+            else if (name === "Z-A")    AZ.checked = false;
+            else if (name === "Preço+") PD.checked = false;
+            else if (name === "Preço-") PU.checked = false;
+        }
+    });
+
 
     $('.js-check :radio').change(function () {
         var check_attr_name = $(this).attr('name');
@@ -33,16 +51,17 @@ $(document).ready(function() {
     });
 
 
-    $('.js-check :checkbox').change(function () {
-        var check_attr_name = $(this).attr('name');
-        if ($(this).is(':checked')) {
-            $(this).closest('.js-check').addClass('active');
-           // item.find('.radio').find('span').text('Add');
-        } else {
-            $(this).closest('.js-check').removeClass('active');
-            // item.find('.radio').find('span').text('Unselect');
-        }
-    });
+    // $('.js-check :checkbox').change(function () {
+    //     console.log('here!')
+    //     var check_attr_name = $(this).attr('name');
+    //     if ($(this).is(':checked')) {
+    //         $(this).closest('.js-check').addClass('active');
+    //        // item.find('.radio').find('span').text('Add');
+    //     } else {
+    //         $(this).closest('.js-check').removeClass('active');
+    //         // item.find('.radio').find('span').text('Unselect');
+    //     }
+    // });
 
 
 
